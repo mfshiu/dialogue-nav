beb6fbdf2e34b3774213d5c5ce832ee1d362f9b0
eric <avatar.xu@gmail.com>
2021-10-28 14:26:49 +0800

    Information.get_indoor_destination_text(k["name"])

--- dialogue/DialogueClient.py
+++ dialogue/DialogueClient.py
@@ -1,3 +1,4 @@
+import os
 import queue
 from dialogue import Speaker, UserListener, Information, UserProcessor, Dialogflow, NavHelper
 
@@ -81,6 +82,11 @@ class DialogueClient:
     def start(self):
         Speaker.play_async("HI，我叫小美，你好。")
         # Speaker.play_async("HI How are you")
+
+        if not os.path.exists("user"):
+            os.makedirs("user")
+        if not os.path.exists("output"):
+            os.makedirs("output")
 
         self._user_listener = UserListener.UserListener(args=(self.user_words,))
         self._user_listener.start()
